88f58fcf8cb4e761ce1e9b3028eac1f0a71fd785
Jang Hyub Seo <seojanghyeob@gmail.com>
2020-03-24 17:20:23 +0900

    add of roles and checking if it is made in english

--- src/main/java/kr/kro/jhseo1107/discord/CommandEvent.java
+++ src/main/java/kr/kro/jhseo1107/discord/CommandEvent.java
@@ -3,8 +3,10 @@ package kr.kro.jhseo1107.discord;
 import kr.kro.jhseo1107.Main;
 import kr.kro.jhseo1107.translation.TranslationString;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
@@ -12,6 +14,7 @@ import org.bukkit.command.Command;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
@@ -27,6 +30,9 @@ public class CommandEvent extends ListenerAdapter {
         if (event.getAuthor().isBot()) return;
         Message message = event.getMessage();
         String content = message.getContentRaw();
+        Role role = event.getGuild().getRoleById(Main.roleid);
+        Member member = event.getGuild().getMember(event.getAuthor());
+        List<Role> member_roles = member.getRoles();
         if (content.contains("!" + Main.dccommand)) {
             MessageChannel channel = event.getChannel();
             String[] args = content.split(" ");
@@ -39,25 +45,40 @@ public class CommandEvent extends ListenerAdapter {
                 }
                 else
                 {
-                    channel.sendMessage(TranslationString.registerstart[Main.langint] + args[1]).queue();
-                    String commandtext = Main.mccommand;
-                    commandtext = commandtext.replace("NICKNAME",args[1]);
-                    final String commandfinal = commandtext;
-                    try {
-                        boolean success = Bukkit.getScheduler().callSyncMethod( main, new Callable<Boolean>() {
-                            @Override
-                            public Boolean call() {
-                                return Bukkit.dispatchCommand( Bukkit.getConsoleSender(),commandfinal );
-                            }
-                        } ).get();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
+                    if(member_roles.contains(role))
+                    {
+                        channel.sendMessage(TranslationString.registerfail[Main.langint]+TranslationString.wrongrole[Main.langint]).queue();
+                        return;
                     }
-
-                    channel.sendMessage(TranslationString.registerdone[Main.langint]+Main.serverip).queue();
-                    return;
+                    else
+                    {
+                        if(args[1].matches("[a-zA-Z0-9]*"))
+                        {
+                            channel.sendMessage(TranslationString.registerstart[Main.langint] + args[1]).queue();
+                            String commandtext = Main.mccommand;
+                            commandtext = commandtext.replace("NICKNAME",args[1]);
+                            final String commandfinal = commandtext;
+                            try {
+                                boolean success = Bukkit.getScheduler().callSyncMethod( main, new Callable<Boolean>() {
+                                    @Override
+                                    public Boolean call() {
+                                        return Bukkit.dispatchCommand( Bukkit.getConsoleSender(),commandfinal );
+                                    }
+                                } ).get();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            } catch (ExecutionException e) {
+                                e.printStackTrace();
+                            }
+                            event.getGuild().addRoleToMember(member, role).queue();
+                            channel.sendMessage(TranslationString.registerdone[Main.langint]+Main.serverip).queue();
+                            return;
+                        }
+                        else
+                        {
+                            channel.sendMessage(TranslationString.registerfail[Main.langint]+TranslationString.specialletters[Main.langint]).queue();
+                        }
+                    }
                 }
             } else {
                 channel.sendMessage(TranslationString.registerfail[Main.langint]+TranslationString.wrongchannel[Main.langint]).queue();
--- src/main/java/kr/kro/jhseo1107/translation/TranslationString.java
+++ src/main/java/kr/kro/jhseo1107/translation/TranslationString.java
@@ -3,12 +3,13 @@ package kr.kro.jhseo1107.translation;
 import net.dv8tion.jda.api.entities.Message;
 
 public class TranslationString {
-    public static final String[] registerfail = new String[]{"Registration Fail.. ", "등록 실패. "};
-    public static final String[] notenougharguments = new String[]{"Not Enough Arguments.", "명령어가 완전하지 않습니다."};
+    public static final String[] registerfail = new String[]{"Registration fail.. ", "등록 실패. "};
+    public static final String[] notenougharguments = new String[]{"Not enough arguments.", "명령어가 완전하지 않습니다."};
     public static final String[] trytext = new String[]{"Try !", "다음 명령어를 시도해보세요: !"};
     public static final String[] minecraftnickname = new String[]{" <Minecraft Nickname>", " <마인크래프트 닉네임>"};
     public static final String[] registerstart = new String[]{"Registration Start : ", "등록 시작 : "};
     public static final String[] registerdone = new String[]{"Registration is done. Server ip : ", "등록 성공. 서버 아이피 : "};
     public static final String[] wrongchannel = new String[]{"Wrong Channel.", "채널이 올바르지 않습니다."};
-    public static final String[] wrongrole = new String[]{"Wrong Role.", "역할이 올바르지 않습니다."};
+    public static final String[] wrongrole = new String[]{"You are already a user.", "이미 유저입니다."};
+    public static final String[] specialletters = new String[]{"Write a nickname made of alphabets and numbers.", "영문과 숫자로 이루어진 닉네임을 입력하세요."};
 }
